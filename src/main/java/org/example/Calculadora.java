a532fe48c4ac4be63d978665d09d135e22f4f244
Stela <stelamontenegro21@gmail.com>
2026-08-26 19:37:03 -0300

    adjusted pipeline with tests, QA and secutrity verification

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -2,17 +2,21 @@ package org.example;
 
 public class Calculadora {
 
-  private Calculadora() {}
+    private Calculadora() {}
 
-  public static Double sum(Double a, Double b) {
-    return a + b;
-  }
-  public static Double sub(Double a, Double b) {
+    public static Double sum(Double a, Double b) {
+        int variavelInutil = 100;
+        return a + b;
+    }
+
+    public static Double sub(Double a, Double b) {
         return a - b;
-  }
+    }
+
     public static Double mult(Double a, Double b) {
         return a * b;
     }
+
     public static Double div(Double a, Double b) {
         return a / b;
     }
