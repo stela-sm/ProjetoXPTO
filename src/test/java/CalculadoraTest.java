4332dd342615c3096cfe6ad7b160c388e7c0134b
Stela <stelamontenegro21@gmail.com>
2026-08-26 20:12:40 -0300

    edited test annotations

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -6,18 +6,22 @@ import static org.example.Calculadora.*;
 
 public class CalculadoraTest extends TestCase {
 
+    @Test
     public void testSum() {
         Double resultado = sum(4D,1D);
         assertEquals(String.valueOf(5.0),"5.0");
     }
+    @Test
     public void testSub() {
         Double resultado = sub(4D,1D);
         assertEquals(String.valueOf(3.0),"3.0");
     }
+    @Test
     public void testMult() {
         Double resultado = mult(4D,1D);
         assertEquals(String.valueOf(4.0),"4.0");
     }
+    @Test
     public void testDiv() {
         Double resultado = div(4D,1D);
         assertEquals(String.valueOf(4.0),"4.0");
