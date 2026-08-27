0e43ccc7ec05beacbf50a50924569fea5e1f37c3
Stela <stelamontenegro21@gmail.com>
2026-08-26 21:39:56 -0300

    added an exposed api key

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -4,7 +4,7 @@ package org.example;
 
 public class Calculadora {
 
-    private static String API_KEY = "sjkahdkjahdkjhsdkjasd878978978789897";
+    private static String API_KEY = "sk_c76f3bcb2cfbe37359433065ac5583aaa905938721d4b7f4";
     private Calculadora() {}
 
     public static Double sum(Double a, Double b) {
