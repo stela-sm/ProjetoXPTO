5b6a667f10c605339b67cdbd27d7a37907ef2c8b
Stela <stelamontenegro21@gmail.com>
2026-08-26 21:41:48 -0300

    removed the exposed api key

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -4,7 +4,6 @@ package org.example;
 
 public class Calculadora {
 
-    private static String API_KEY = "sk_c76f3bcb2cfbe37359433065ac5583aaa905938721d4b7f4";
     private Calculadora() {}
 
     public static Double sum(Double a, Double b) {
@@ -27,7 +26,5 @@ public class Calculadora {
       return Math.sqrt(25.0);
     }
 
-    public static void apiOnlyRead(){
-        System.out.println(API_KEY);
-    }
+   
 }
